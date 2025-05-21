9df11a1150e5d923c490d192a06c7c90e85baac3
Esteban <esteban.morauc@gmail.com>
2025-05-21 13:28:08 -0400

    Creación del microservicio autentificacion

---
+++ src/test/java/com/grupo6/autentificacion/AutentificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.autentificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutentificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
